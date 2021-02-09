2c9cc0d12cf7c8d1887aa907cdc22d5f28dfcaad
Liwu-di <liwudi@liwudi.fun>
2021-02-09 10:31:18 +0800

    创建毕设模块

--- eureka-client/src/main/java/fun/liwudi/eurekaclient/rest/LoginRest.java
+++ eureka-client/src/main/java/fun/liwudi/eurekaclient/rest/LoginRest.java
@@ -1,17 +1,29 @@
 package fun.liwudi.eurekaclient.rest;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author 张安熠
+ * @author 李武第
  */
 @RestController
 public class LoginRest {
-    @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public String login(){
-        return null;
+
+    @RequestMapping("/login")
+    public String login(@RequestParam("name") String name,@RequestParam("password") String password){
+       if(password.equals("lwd")) {
+           return "success to login";
+       }
+       else{
+           return "fail to login";
+       }
+    }
+
+    public static void main(String[] args) {
+        LoginRest loginRest = new LoginRest();
+        System.out.println(loginRest.login("anyi","anyi"));
     }
 }
+
+
